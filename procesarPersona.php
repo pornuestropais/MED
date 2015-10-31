6eccd91bb29310b3682a60f5d2f7bf7215103b1e
jrozon <jrozon@avathartech.com>
2015-10-31 16:16:21 -0400

    Arreglando persona

--- procesarPersona.php
+++ procesarPersona.php
@@ -1,18 +1,30 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: jorda
+ * Date: 10/31/2015
+ * Time: 1:31 PM
+ */
+?>
+<?php
 
 require_once "ConexionDB.php";
 
+//Estacion 1
 $conexion->query( "
-		INSERT INTO
+		UPDATE
 			persona
-		VALUES(
-				IdPersona = '".$_POST['IdPersona']."',
-				nombre = '".$_POST[ 'nombre' ]."',
-				edad = '".$_POST[ 'edad' ]."',
-				sexo = '".$_POST[ 'sexo' ]."',
-				direccion = '".$_POST[ 'direccion' ]."',
-				estado_civil = '".$_POST[ 'estado_civil' ]."',
-				hijos = '".$_POST[ 'hijos' ]."')");
+		SET
+			nombre = '".$_POST[ 'nombre' ]."',
+			edad = '".$_POST[ 'edad' ]."',
+			sexo = '".$_POST[ 'sexo' ]."',
+			direccion = '".$_POST[ 'direccion' ]."',
+			estado_civil = '".$_POST[ 'estado_civil' ]."',
+			hijos = '".$_POST[ 'hijos' ]."'
+		WHERE
+			IdPersona = '".$_POST[ 'IdPersona' ]."'
+	" );
+
 
 //Insertar medicamentos
 #foreach ( $_POST[ 'medicamentos' ] as $trash => $medicamento ) {
@@ -20,4 +32,5 @@ $conexion->query( "
 #}
 
 header( 'Location: '. $direccion.'persona.php?id='.$_POST[ 'id' ] );
+
 ?>
